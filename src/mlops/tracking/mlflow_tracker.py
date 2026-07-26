aecf835d03a2a61cbc52128f80f5fc27b4009072
Mohammed Ramzi Abdallah <radx.abdel@gmail.com>
2026-07-26 22:24:19 +0100

    fix(mlops): fixing types in mlflow_tracker.py that shows in mypy tests

--- src/mlops/tracking/mlflow_tracker.py
+++ src/mlops/tracking/mlflow_tracker.py
@@ -1,22 +1,25 @@
+from pathlib import Path
+from typing import Any
+
 from .base import ExperimentTracker
 
 
 class MLflowTracker(ExperimentTracker):
 
-    def start_run(self, run_name=None):
+    def start_run(self, run_name: str | None = None) -> None:
         raise NotImplementedError
 
-    def log_parameters(self, parameters):
+    def log_parameters(self, parameters: dict[str, Any]) -> None:
         raise NotImplementedError
 
-    def log_metrics(self, metrics):
+    def log_metrics(self, metrics: dict[str, int | float]) -> None:
         raise NotImplementedError
 
-    def log_artifact(self, artifact):
+    def log_artifact(self, artifact: str | Path) -> None:
         raise NotImplementedError
 
-    def log_model(self, model):
+    def log_model(self, model: Any) -> None:
         raise NotImplementedError
 
-    def end_run(self):
+    def end_run(self) -> None:
         raise NotImplementedError
