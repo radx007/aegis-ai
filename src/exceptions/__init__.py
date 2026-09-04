7b0ba8072fcae4fb9018e334e798184626eafbad
Mohammed Ramzi Abdallah <radx.abdel@gmail.com>
2026-09-04 02:54:54 +0100

    deleting modelError dead code

--- src/exceptions/__init__.py
+++ src/exceptions/__init__.py
@@ -1,7 +1,6 @@
 from .base import AegisError
 from .dataset import DatasetError
 from .embedding import EmbeddingError
-from .model import ModelError
 from .model_loading import ModelLoadingError
 from .prediction import PredictionError
 from .training import TrainingError
@@ -10,7 +9,6 @@ __all__ = [
     "AegisError",
     "DatasetError",
     "EmbeddingError",
-    "ModelError",
     "PredictionError",
     "TrainingError",
     "ModelLoadingError",
--- src/exceptions/model.py
+++
@@ -1,5 +0,0 @@
-from .base import AegisError
-
-
-class ModelError(AegisError):
-    """Raised when model persistence fails."""
